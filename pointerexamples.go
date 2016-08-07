a357100bd00890f95769178231ffdb3476979f48
akosgarai <szlem01@gmail.com>
2016-08-07 12:11:10 +0200

    refactor

--- pointerexamples.go
+++ pointerexamples.go
@@ -6,6 +6,10 @@ import (
 
 func main () {
     i, j := 42, 2701
+    pointerExample(i, j)
+}
+
+func pointerExample (i, j int) {
     p := &i     //point to i
     fmt.Println(*p) // read i through the pointer
     *p = 21         // set i through the pointer
