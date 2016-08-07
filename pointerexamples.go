d1a929945696192c06ecc903281894d93e050f8a
akosgarai <szlem01@gmail.com>
2016-08-07 13:18:00 +0200

    example functions for making slices - added

--- pointerexamples.go
+++ pointerexamples.go
@@ -16,6 +16,7 @@ func main () {
     arrayExample()
     arrayExample_2()
     sliceLiteralsExample()
+    makingSlicesExample()
 }
 
 func vertexExample (i, j int) {
@@ -87,4 +88,22 @@ func sliceLiteralsExample () {
         {13, true},
     }
     fmt.Println(s)
+}
+
+func makingSlicesExample() {
+    a := make([]int, 5)
+    printSlice("a", a)
+
+    b := make([]int, 0, 5)
+    printSlice("b", b)
+
+    c := b[:2]
+    printSlice("c", c)
+
+    d := c[2:5]
+    printSlice("d", d)
+}
+
+func printSlice(s string, x []int) {
+    fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
 }
