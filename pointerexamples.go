33bc5c06614d45ec442ead0e6fedef51fe464f78
akosgarai <szlem01@gmail.com>
2016-08-07 12:36:42 +0200

    array example functions added

--- pointerexamples.go
+++ pointerexamples.go
@@ -13,6 +13,8 @@ func main () {
     i, j := 42, 2701
     pointerExample(i, j)
     vertexExample(i, j)
+    arrayExample()
+    arrayExample_2()
 }
 
 func vertexExample (i, j int) {
@@ -22,6 +24,36 @@ func vertexExample (i, j int) {
     fmt.Println(*p)
     p.X = 0
     fmt.Println(*p)
+}
+
+func arrayExample_2 () {
+    names := [4]string{
+        "John",
+        "Paul",
+        "George",
+        "Ringo",
+    }
+    fmt.Println(names)
+
+    a := names[0:2]
+    b := names[1:3]
+    fmt.Println(a, b)
+
+    b[0] = "XXX"
+    fmt.Println(a, b)
+    fmt.Println(names)
+}
+func arrayExample () {
+    var a [2]string
+    a[0] = "Hello"
+    a[1] = "World"
+    fmt.Println(a[0], a[1])
+    fmt.Println(a)
+
+    primes := [6]int{2, 3, 5, 7, 11, 13}
+    fmt.Println(primes)
+    var s []int = primes[1:4]
+    fmt.Println(s)
 }
 
 func pointerExample (i, j int) {
